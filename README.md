b484324faee8d3e7d7e83bf22ab3b7d778c6d13d
Human <reedwith2es@gmail.com>
2023-04-07 23:04:35 -0700

    Working and moderately documented e2e tests

--- README.md
+++ README.md
@@ -46,24 +46,55 @@ All of the `*.test.ts` files will run, and their output will be shown together.
 
 By default, Vitest runs in "watch mode".
 
+## End-to-End Tests
+
+End-to-end tests (`e2e`) in this context mean tests which use a real browser with
+the interface to Hyperscript that real users (developers) will use.
+
+The `index.html` page is the surface on which the e2e tests run. Human beings
+can open the page and look at it and click the buttons, etc. That is the
+exact behavior the e2e tests emulate.
+
+The e2e framework I'm using is [Playwright](https://www.playwright.dev).
+
+The e2e test code lives in `e2e-tests/*.spec.ts`
+
+### Running e2e Tests
+
+All you have to do is `npm run e2e`. 
+
+The `webServer` setting in `playwright.config.ts` causes Playwright to run the 
+Vite `build` and `preview` commands instead of the dev server, so that it's
+testing exactly what end users see on their browsers.
+
+If you'd like to test against the dev server instead for speed, see the comment
+in `playwright.config.ts`.
+
+To run the e2e tests in watch mode, use `npm run e2e:ui` to open the Playwright
+test UI, and use the "watch" button there.
+
 ## Strategy
 
 My general strategy for adding new features is to start with wishful thinking:
 
-1. write some code which I wish worked in `index.html`
-1. see that it doesn't work yet
-1. write a failing test in `parser.test.js`,
-1. make it pass.
-1. commit
-1. write a failing test in `transpiler.test.js`,
-1. make it pass
-1. ammend my commit
-1. write a failing test in `runtime.test.js`,
-1. make it pass
-1. ammend my commit
-1. make what I wrote in `index.html` work
-1. ensure all the tests still pass
-1. ammend my commit  
+1. Write some code which I wish worked in `index.html`
+1. Manually check that it doesn't work yet
+1. Write a failing test in `parser.test.js`,
+1. Make it pass.
+1. Commit
+1. Write a failing test in `transpiler.test.js`,
+1. Make it pass
+1. Amend my commit
+1. Write a failing test in `runtime.test.js`,
+1. Make it pass
+1. Amend my commit
+1. Write a failing test in `e2e-tests/*.spec.ts`,
+1. Make it pass
+1. Amend my commit
+1. Manually check that what I wrote in `index.html` works
+1. Ensure all the tests still pass
+1. Amend my commit
+1. Push.
 
 ## Decisions
 
---
+++ e2e-tests/hyperscript-buttons.spec.ts
@@ -0,0 +1,120 @@
+import { test, expect } from '@playwright/test';
+import path from 'path';
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// Playwright `expect().toHaveCSS()` apparently uses `getComputedStyle()` which
+// always reports in this `rgb()` format
+const BLACK_CSS = "rgb(33, 53, 71)";
+const GREEN_CSS = "rgb(0, 128, 0)";
+
+test('has title', async ({ page }, testInfo) => {
+    await page.goto(`http://localhost:${testInfo?.config?.webServer?.port}/`);
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Hyperscript/);
+});
+
+test('log button logs', async ({ page }, testInfo) => {
+    await page.goto(`http://localhost:${testInfo?.config?.webServer?.port}/`);
+
+    // When anything logs to the console, the flag will be flipped
+    let logCount = 0;
+    page.on('console', (msg) => {
+        // Ignore Vite dev message
+        if (msg.text().match(/vite.*connected/)) return;
+        logCount += 1
+    });
+
+    // Click the log button.
+    const indicator = page.locator('[hs-indicate-log]');
+    const container = page.locator('tr', { has: indicator })
+    const button = container.getByRole('button')
+    await button.click();
+
+    // Expects the URL to contain intro.
+    await expect(logCount).toBe(1);
+});
+
+test('JS call button turns the indicator red', async ({ page }, testInfo) => {
+    await page.goto(`http://localhost:${testInfo?.config?.webServer?.port}/`);
+
+    // Click the log button.
+    const indicator = page.locator('[hs-indicate-global-js-call]');
+    const container = page.locator('tr', { has: indicator })
+    const button = container.getByRole('button')
+    await button.click();
+
+    // Expects the URL to contain intro.
+    await expect(indicator).toHaveCSS('color', GREEN_CSS)
+});
+
+test('Use `next` expression', async ({ page }, testInfo) => {
+    await page.goto(`http://localhost:${testInfo?.config?.webServer?.port}/`);
+
+    // Click the log button.
+    const indicator = page.locator('[hs-indicate-next-expression]');
+    const container = page.locator('tr', { has: indicator })
+    const button = container.getByRole('button')
+    await button.click();
+
+    // Expects the URL to contain intro.
+    await expect(indicator).toHaveCSS('color', GREEN_CSS)
+});
+
+test('Set color style', async ({ page }, testInfo) => {
+    await page.goto(`http://localhost:${testInfo?.config?.webServer?.port}/`);
+
+    // Click the log button.
+    const indicator = page.locator('[hs-indicate-set-color-style]');
+    const container = page.locator('tr', { has: indicator })
+    const button = container.getByRole('button')
+    await button.click();
+
+    // Expects the URL to contain intro.
+    await expect(indicator).toHaveCSS('color', GREEN_CSS)
+});
+
+test('Multi-line on-click feature', async ({ page }, testInfo) => {
+    await page.goto(`http://localhost:${testInfo?.config?.webServer?.port}/`);
+
+    // Click the log button.
+    const indicator = page.locator('[hs-indicate-multi-line]');
+    const container = page.locator('tr', { has: indicator })
+    const button = container.getByRole('button')
+    await button.click();
+
+    // Expects the URL to contain intro.
+    await expect(indicator).toHaveCSS('color', GREEN_CSS)
+});
+
+test('Delay 1 second', async ({ page, context }, testInfo) => {
+    // From https://github.com/microsoft/playwright/issues/6347#issuecomment-1092881462
+    // Insert sinon into the browser so we can mess with timers
+    const sinonPath = path.join(__dirname, '..', './node_modules/sinon/pkg/sinon.js');
+    await context.addInitScript({
+        path: sinonPath,
+    });
+    await context.addInitScript(() => {
+        //@ts-ignore This code is run in browser, so not going to try to type it
+        window.__clock = sinon.useFakeTimers();
+    });
+    await page.goto(`http://localhost:${testInfo?.config?.webServer?.port}/`);
+
+    // Click the log button.
+    const indicator = page.locator('[hs-indicate-delay]');
+    const container = page.locator('tr', { has: indicator })
+    const button = container.getByRole('button')
+    await button.click();
+
+
+    // Ensure controlled time
+    await expect(indicator).toHaveCSS('color', BLACK_CSS, { timeout: 1 /* ms */ })
+    await page.evaluate(() => {
+        //@ts-ignore This code is run in browser, so not going to try to type it
+        window.__clock.tick(1000)
+    });
+    await expect(indicator).toHaveCSS('color', GREEN_CSS, { timeout: 1 /* ms */ })
+});
